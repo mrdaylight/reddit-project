b6290b12aa3681da2098a209005fad0e093c8acb
mrdaylight <mrdaylight@protonmail.com>
2020-02-08 22:19:13 +0000

    vote progress, no refresh

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+	votes:number;
+	title:string;
+	link:string;
 
-  constructor() { }
+  constructor() {
+	this.title="Angular";
+	this.title="http://angular.io"; 	
+	this.votes=10;
+  }
 
   ngOnInit() {
   }
+
+voteUp():boolean {
+this.votes ++;
+return false;
+}
+
+voteDown():boolean {
+this.votes --;
+return false;
+}
 
 }
