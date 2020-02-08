037700d30d630ffccee19321653680cded9a0ab3
mrdaylight <mrdaylight@protonmail.com>
2020-02-08 21:44:47 +0000

    progress to lab3, article formed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reddit-project';
+	addArticle(title:HTMLInputElement, link:HTMLInputElement):Boolean {
+		console.log(`Adding aritcle Title:${title.value} and adding article Link:${link.value} `);
+		console.log('test');
+		return false;
+	}
 }
